7527bf1da9d122c1c3b4b61a33a008e8bdfe3634
jeffreychu0 <jchu08@nyit.edu>
2026-04-29 00:19:04 -0400

    Question 3 Submission

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,21 @@ function App2() {
     );
 }
 
+// Question Number 3
+function App3() {
+    const [alertId, setAlertId] = useState("hidden")
+    return (
+        <div>
+            <div data-testid="alert" id={alertId}>
+                <h2>Are you sure?</h2>
+                <p>These changes can't be reverted!</p>
+                <button onClick={() => { setAlertId("hidden") }}>Cancel</button>
+                <button onClick={() => { setAlertId("hidden") }}>Proceed</button>
+            </div>
+            <button onClick={() => { setAlertId("alert") }}>Delete</button>
+        </div>
+    );
+}
 
 
 export default App2;
